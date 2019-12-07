2f3cf8d9d04afd2986e5153d70ad3e18ab358d00
Furkan Cabbar <ffcabbar@gmail.com>
2019-12-07 15:10:40 +0300

    Updates

---
+++ precache-manifest.ed8bc4ca3983444e997d6084428dddd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcd57527ea18c9e20cecbc899514aaf",
+    "url": "/pets-app/index.html"
+  },
+  {
+    "revision": "7a8ef0e897b38e2b3034",
+    "url": "/pets-app/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "087279f10e446b218648",
+    "url": "/pets-app/static/css/main.f361cb12.chunk.css"
+  },
+  {
+    "revision": "7a8ef0e897b38e2b3034",
+    "url": "/pets-app/static/js/2.c24897d9.chunk.js"
+  },
+  {
+    "revision": "087279f10e446b218648",
+    "url": "/pets-app/static/js/main.2116f5cc.chunk.js"
+  },
+  {
+    "revision": "72ad3711a7adb948b794",
+    "url": "/pets-app/static/js/runtime-main.2857c87e.js"
+  }
+]);
